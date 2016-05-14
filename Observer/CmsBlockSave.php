85c05a21e245cbe7a682b40cb8c73c6f41158ec1
m2dev on mist.magehost.pro <m2dev@mist.magehost.pro>
2016-05-14 16:26:30 +0200

    Finished prototype observer

--- Observer/CmsBlockSave.php
+++ Observer/CmsBlockSave.php
@@ -1,40 +1,45 @@
 <?php
 
 namespace Hackathon\VarnishImprovement\Observer;
+use Magento\Framework\App\PageCache\Cache;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Widget\Model\ResourceModel\Widget\Instance\Collection;
 
 class CmsBlockSave implements ObserverInterface
 {
-    protected $_moduleData;
-    protected $_registry = null;
-    protected $_widgetCollection = null;
+    protected $widgetCollection = null;
+    protected $cache = null;
 
     public function __construct (
-        \Magento\Framework\Registry $registry,
-        \Magento\Widget\Model\ResourceModel\Widget\Instance\Collection $widgetCollection
+        Collection $widgetCollection,
+        Cache $cache
     ) {
-        $this->_registry = $registry;
-        $this->_widgetCollection = $widgetCollection;
+        $this->widgetCollection = $widgetCollection;
+        $this->cache = $cache;
     }
 
     public function execute(Observer $observer)
     {
         /** @var \Magento\Cms\Model\Block $cmsBlock */
         $cmsBlock = $observer->getObject();
-        $ident = $cmsBlock->getIdentifier();
         /** @var \Magento\Widget\Model\Widget\Instance $widget */
-        foreach ( $this->_widgetCollection as $widget ) {
+        $needFlush = false;
+        // Dirty prototype double foreach loop
+        foreach ( $this->widgetCollection as $widget ) {
             $param = $widget->getWidgetParameters();
             if ( $param['block_id'] == $cmsBlock->getId() ) {
                 $widget->load( $widget->getId() );
                 foreach ( $widget->getPageGroups() as $pageGroup ) {
-                    if ( 'cms_footer_links_container' == $pageGroup['block_reference'] ) {
-                        // Flush in Varnish
+                    if ( 'wrapped_cms_footer_links_container' == $pageGroup['block_reference'] ) {
+                        $needFlush = true;
                     }
                 }
 
             }
+        }
+        if ($needFlush) {
+            $this->cache->clean( 'footer_links_wrapper' );
         }
     }
 
