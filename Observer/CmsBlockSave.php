73e0318fe52476c74776b980131012058a6be52a
m2dev on mist.magehost.pro <m2dev@mist.magehost.pro>
2016-05-14 17:00:19 +0200

    Improving flush

--- Observer/CmsBlockSave.php
+++ Observer/CmsBlockSave.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Hackathon\VarnishImprovement\Observer;
+use Magento\CacheInvalidate\Model\PurgeCache;
 use Magento\Framework\App\PageCache\Cache;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
@@ -9,14 +10,15 @@ use Magento\Widget\Model\ResourceModel\Widget\Instance\Collection;
 class CmsBlockSave implements ObserverInterface
 {
     protected $widgetCollection = null;
-    protected $cache = null;
+    protected $purgeCache = null;
 
     public function __construct (
         Collection $widgetCollection,
-        Cache $cache
+        Cache $cache,
+        PurgeCache $purgeCache
     ) {
         $this->widgetCollection = $widgetCollection;
-        $this->cache = $cache;
+        $this->purgeCache = $purgeCache;
     }
 
     public function execute(Observer $observer)
@@ -39,7 +41,11 @@ class CmsBlockSave implements ObserverInterface
             }
         }
         if ($needFlush) {
-            $this->cache->clean( 'footer_links_wrapper' );
+            error_log("Flushed");
+            //$this->purgeCache->sendPurgeRequest('footer_links_wrapper');
+            $this->purgeCache->sendPurgeRequest('((^|,)footer_links_wrapper(,|$))');
+            // Dirty prototype fix
+            //shell_exec('varnishadm \'ban  obj.http.X-Magento-Tags ~ "footer_links_wrapper"\'');
         }
     }
 
